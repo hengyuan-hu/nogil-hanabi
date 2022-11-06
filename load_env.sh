edccef25248ec9c8f8445f2fb4e9fe2e3f12a092
Hengyuan Hu <hengyuan.hhu@gmail.com>
2022-11-05 23:08:35 -0700

    remove file

--- load_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-# module load anaconda3/2021.05
-module load cudnn/v7.6.5.32-cuda.10.2
-module load cuda/10.2
-
-# source activate /private/home/hengyuan/.conda/envs/pth1.10.2
-export OMP_NUM_THREADS=1
